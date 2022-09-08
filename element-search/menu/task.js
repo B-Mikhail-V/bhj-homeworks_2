d68c93ee6c7960b2f09c551037b6030dcae79c55
B-Mikhail-V <bakulin.mv@gmail.com>
2022-09-08 21:46:35 +0300

    Task2-2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,23 +1,40 @@
 const menuLink = document.querySelectorAll('.menu__link')
-console.log(menuLink);
-arrMenu = Array.from(menuLink)
+// console.log(menuLink.length);
+// console.log(menuLink[0].href);
+Array.from(menuLink).forEach(element => {
+  console.log(element.className);
+  console.log(element.closest('.menu_sub'))
 
-console.log(arrMenu);
+  // if(!element.href.includes('html')) {
+  //   // console.log(element.closest('.menu_sub'));
+  //   console.log(element.parentElement.parentElement.className);
+  //    console.log(element.href); 
+    //  element.parentElement.parentElement.className = 'menu_active'
+    //  element.closest('.menu_sub').className = 'menu_active'
 
-arrMenu.forEach(element => {
-        // console.log(element.href.includes('html'));
+  }
 
-    if (element.closest('.menu_sub')) {
-        // console.log(element.closest('.menu_sub'));
-        // console.log(element.closest('.menu_sub').className);
+);
 
-          element.onclick = () => {
-        element.closest('.menu_sub').textContent = 'menu_active';
-        // console.log(element.closest('.menu_sub').className);
-    }  
-    }
 
-    // console.log(element.href.includes('netology'));
-});
+// arrMenu = Array.from(menuLink)
+
+// console.log(arrMenu);
+
+// arrMenu.forEach(element => {
+//         // console.log(element.href.includes('html'));
+
+//     if (element.closest('.menu_sub')) {
+//         // console.log(element.closest('.menu_sub'));
+//         // console.log(element.closest('.menu_sub').className);
+
+//           element.onclick = () => {
+//         element.closest('.menu_sub').textContent = 'menu_active';
+//         // console.log(element.closest('.menu_sub').className);
+//     }  
+//     }
+
+//     // console.log(element.href.includes('netology'));
+// });
 
 
