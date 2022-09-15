08ab97895b08af6f044bd9519785f18d61b87e30
Mikail Bakulin <bakulin.mv@gmail.com>
2022-09-15 17:58:21 +0300

    Task_2_corr

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,42 +1,15 @@
 const sliderItem = document.querySelectorAll('.slider__item')
-const arr1 = Array.from(sliderItem)
+arr1 = Array.from(sliderItem)
+console.log(arr1);
 
-function getCounter(start) {
-    let counter = start
-    return function (pl) {
-            switch(pl){
-            case '+': result = counter++ % sliderItem.length;
-            break;
-            case '-': result = counter-- % sliderItem.length;
-            break;
-            }    
 
-        return result;      
-    }
-    
+
+function activeSlide() {
+   const actSl = document.querySelector('.slider__item_active') 
+   return actSl;
 } 
-counter1 = getCounter(100000)
 
-function deactiveSlider(arr) {
-    const allSubMenu = document.querySelectorAll('.menu_active');
-    arr.forEach(element => {
-      element.classList.remove('slider__item_active')
-    })
-  }
+acc = activeSlide()
+console.log(acc);
 
-
-const nextClick = document.querySelector('.slider__arrow_next')
-nextClick.onclick = () => {
-    console.log(counter1('+'));
-    // deactiveSlider(arr1)
-    // sliderItem[counter1('+')].classList.add('slider__item_active')
-}
-
-const prevClick = document.querySelector('.slider__arrow_prev')
-prevClick.onclick = () => {
-    currCount = counter1('-') 
-    counter2 = getCounter(currCount - 2)
-    console.log(counter2('-'));
-    // deactiveSlider(arr1)
-    // sliderItem[counter1('-')].classList.add('slider__item_active')
-}
+console.log(arr1.findIndex(activeSlide));
